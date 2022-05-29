33dcfc351c7d9a3f06837a0cdf06d990eca28d92
Armando Cruz <jaimes087.uz@gmail.com>
2022-05-29 17:55:38 -0500

    fix: fix tob-bar title

--- src/components/tab-bar/tab-bar.tsx
+++ src/components/tab-bar/tab-bar.tsx
@@ -6,7 +6,7 @@ export const TabBar = (props: Props) => {
   const { title } = props
   return (
     <header className="px-4 py-3 bg-[#1e1f23]">
-      <h2 className="uppercase">{title}</h2>
+      <h2 className="text-lg m-0 tracking-wider uppercase">{title}</h2>
     </header>
   )
 }
